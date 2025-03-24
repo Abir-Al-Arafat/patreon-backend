af0d6e9c04ebf0ee7197691fefa7431cf0d14047
abir <62530610+Abir-Al-Arafat@users.noreply.github.com>
2025-03-24 13:52:42 +0600

    update: cors and types

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import cors from "cors";
+import { Request, Response, NextFunction } from "express";
 import userRouter from "./routes/users.router";
 
 const app = express();
 dotenv.config();
 
-const PORT = process.env.PORT || 3001;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "*", credentials: true }));
+app.use(express.json()); // Parses data as JSON
+app.use(express.text()); // Parses data as text
+app.use(express.urlencoded({ extended: true })); // Parses data as URL-encoded
+// ✅ Handle Invalid JSON Errors
+app.use(
+  (
+    err: SyntaxError & { status?: number; body?: any },
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+      return res.status(400).send({ message: "Invalid JSON format" });
+    }
+    next();
+  }
+);
 
 app.use("/public", express.static(path.join(__dirname, "public")));
 
@@ -25,6 +41,7 @@ app.get("/", (req, res) => {
   });
 });
 
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

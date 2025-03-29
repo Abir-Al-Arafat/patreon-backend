386caab1b73c517f17365b840040cdc5aa6e5a35
abir <62530610+Abir-Al-Arafat@users.noreply.github.com>
2025-03-29 14:29:17 +0600

    add: user & notification model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
@@ -80,7 +80,7 @@ const userSchema = new mongoose.Schema(
     },
 
     emailVerifyCode: {
-      type: String,
+      type: Number,
     },
 
     isActive: {
@@ -98,5 +98,5 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+// export const User = mongoose.model("User", userSchema);
+export default mongoose.model("User", userSchema);

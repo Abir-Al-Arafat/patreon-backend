765a25618b8d21f840e7027c5f67bdf9c509ab50
abir <62530610+Abir-Al-Arafat@users.noreply.github.com>
2025-04-22 11:51:51 +0600

    cleanup: unnecessary code

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,25 +1,3 @@
-// import { CreateUserQueryParams } from "../types/query-params";
-
-// import { IUser } from "../interfaces/user.interface";
-
-// export function getUsers(request: Request, response: Response) {
-//   response.send([]);
-// }
-
-// export function getUserById(request: Request, response: Response) {
-//   response.send({});
-// }
-
-// export function createUser(
-//   request: Request<{}, {}, IUser, CreateUserQueryParams>,
-//   response: Response<IUser>
-// ) {
-//   return response.status(201).send({
-//     username: "anson",
-//     email: "anson@ansonthedev.com",
-//     name: "Anson",
-//   });
-// }
 import { Request, Response } from "express";
 import { success, failure } from "../utilities/common";
 import { IQuery } from "../types/query-params";

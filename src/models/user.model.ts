2e38b6bfe7f360a3045f1d3f4174ee665c06a457
abir <62530610+Abir-Al-Arafat@users.noreply.github.com>
2025-06-02 18:03:47 +0600

    update: stripe account id

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -34,6 +34,10 @@ const userSchema = new mongoose.Schema(
       type: String,
     },
 
+    stripeAccountId: {
+      type: String,
+    },
+
     subscriptions: [
       { type: mongoose.Schema.Types.ObjectId, ref: "Subscription" },
     ],
